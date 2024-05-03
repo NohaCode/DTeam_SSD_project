75a7a4fd863de183f48eabe7b19a69d2e664ac2e
jinha <gammazx@naver.com>
2024-05-03 12:43:38 +0900

    first setting

---
+++ src/main/java/SSD.java
@@ -0,0 +1,15 @@
+public class SSD {
+
+    private byte LBA[];
+
+    public SSD() {
+        this.LBA = new byte[100];
+    }
+
+    public void write(int index, byte b){
+    }
+
+    public byte read(int index){
+        return 0;
+    }
+}
