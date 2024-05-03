eca66473148a32f611cc19ab4934eb033a1a5b51
chlduq04 <chlduq04@gmail.com>
2024-05-03 13:06:11 +0900

    SSD Write Unit Test Code

--- src/test/java/SSDTest.java
+++ src/test/java/SSDTest.java
@@ -1,5 +1,36 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class SSDTest {
 
+    @Test
+    void write_SSD_데이터_없는_곳에_쓰기_성공() {
+    }
+
+    @Test
+    void write_SSD_데이터_있는_곳에_쓰기_성공() {
+    }
+
+    @Test
+    void write_SSD_알맞은_범위에_쓰기_성공() {
+    }
+
+    @Test
+    void write_SSD_알맞은_값으로_쓰기_성공() {
+    }
+
+    @Test
+    void write_SSD_알맞지_않은_범위에_쓰기_실패() {
+    }
+
+    @Test
+    void write_SSD_알맞지_않은_값으로_쓰기_실패() {
+    }
+
+    @Test
+    void write_SSD_범위가_누락되어_쓰기_실패() {
+    }
+
+    @Test
+    void write_SSD_값이_누락되어_쓰기_실패() {
+    }
 }
