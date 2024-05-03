14c641180f17cbac4950c3b5274cf5e3041abb3b
dowon-ahn <ahndw55@gmail.com>
2024-05-03 13:19:55 +0900

    fullWrite / fullRead 테스트 케이스 작성

---
+++ src/test/java/FullReadAndWriteTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+public class FullReadAndWriteTest {
+
+    @Test
+    void 전체파일읽기_통과(){
+        
+    }
+
+    @Test
+    void 전체파일읽기_실패(){
+
+    }
+    
+    @Test
+    void 전체파일쓰기_4바이트_입력_통과(){
+        
+    }
+
+
+    @Test
+    void 전체파일쓰기_4바이트아님_입력_실패(){
+        
+    }
+
+}
