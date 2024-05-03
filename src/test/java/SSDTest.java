209004c2fee3fdafbc6f4f9333645cf5f0f4b69c
jinha <gammazx@naver.com>
2024-05-03 13:09:37 +0900

    Unit Test - Shell exit&help

--- src/test/java/SSDTest.java
+++ src/test/java/SSDTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SSDTest {
 
+    @Test
+    void shell_exit_함수호출시_shell_종료확인() {
+    }
+
+    @Test
+    void shell_help_함수호출시_사용방법_출력확인() {
+    }
 }
