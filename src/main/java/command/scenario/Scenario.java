60f58ef2d46265acdc01246f1b8b968e14594acf
chlduq04 <chlduq04@gmail.com>
2024-05-09 08:37:16 +0900

    [refactor] rebuild scenario 함수 추가 및 테스트

--- src/main/java/command/scenario/Scenario.java
+++ src/main/java/command/scenario/Scenario.java
@@ -1,7 +1,5 @@
 package command.scenario;
 
-import app.Shell;
-import exception.ShellException;
 import util.FileHandler;
 
 import java.util.ArrayList;
@@ -10,49 +8,88 @@ import java.util.List;
 
 public class Scenario {
     private FileHandler fileHandler;
+    public static final String LINE_SEPARATOR = "\n";
 
     public Scenario() {
         fileHandler = FileHandler.get();
     }
 
-    public String read(int index) {
-        String allScenarioString = fileHandler.readScenario().trim();
+    public void readAll() {
+        String allScenarioString = readFile();
+        String[] scenarioList = allScenarioString.split(LINE_SEPARATOR);
+        for (int i=0 ; i < scenarioList.length ; i++) {
+            System.out.println("[" + i + "] " + scenarioList[i]);
+        }
+    }
+
+    public void readRange(int start, int end) {
+        String allScenarioString = readFile();
+        String[] scenarioList = allScenarioString.split(LINE_SEPARATOR);
+        int maxLength = scenarioList.length;
+        if (start > end) {
+            return;
+        }
+        int newStartIndex = Math.min(start, maxLength);
+        int newEndIndex = Math.min(end, maxLength);
+        for (; newStartIndex < newEndIndex ; newStartIndex++) {
+            System.out.println("[" + newStartIndex+ "] " + scenarioList[newStartIndex]);
+        }
+    }
+
+
+    private String readFile() {
+        return fileHandler.readScenario().trim();
+    }
+
+    public void read(int index) {
+        String allScenarioString = readFile();
         if (isEmptyScenario(allScenarioString)) {
-            throw new ShellException();
+            System.out.println("테스트 시나리오 없음");
+            return;
         }
-        String[] scenarioList = allScenarioString.split("\n");
+        String[] scenarioList = allScenarioString.split(LINE_SEPARATOR);
         if (isInvalidReadIndex(index, scenarioList)) {
-            throw new ShellException();
+            System.out.println();
+            return;
         }
-        return scenarioList[index];
+        System.out.println("[" + index + "] " + scenarioList[index]);
     }
 
     private static boolean isEmptyScenario(String allScenarioString) {
         return allScenarioString == null || allScenarioString.isEmpty();
     }
 
-    public String readAll() {
-        return fileHandler.readScenario();
-    }
-
     private static boolean isInvalidReadIndex(int index, String[] scenarioList) {
-        return scenarioList.length == 0 || scenarioList.length >= index;
+        return scenarioList.length <= index;
     }
 
-    public void create(String command) {
-        String allContents = readAll();
-        allContents = allContents.trim() + "\n" + command;
-        fileHandler.writeScenario(allContents);
+    private static boolean isInvalidWriteIndex(int index, String[] scenarioList) {
+        return scenarioList.length < index;
+    }
+
+    public void append(String command) {
+        String allScenarioString = readFile();
+        if (isEmptyScenario(allScenarioString)) {
+            allScenarioString = command;
+        } else {
+            allScenarioString = allScenarioString + LINE_SEPARATOR + command;
+        }
+        fileHandler.writeScenario(allScenarioString);
     }
 
     public void insert(int index, String command) {
-        String allContents = readAll();
-        String[] scenarioList = allContents.split("\n");
+        String[] scenarioList = getScenarioList();
+        if (isInvalidWriteIndex(index, scenarioList)) {
+            return;
+        }
         List<String> list = new ArrayList<>(Arrays.asList(scenarioList));
         list.add(index, command);
-        scenarioList = list.toArray(new String[0]);
-        allContents = String.join("\n", scenarioList);
-        fileHandler.writeScenario(allContents);
+        writeScenario(list);
+    }
+
+    private String[] getScenarioList() {
+        String allScenarioString = readFile();
+        return allScenarioString.split(LINE_SEPARATOR);
     }
 
     public void deleteAll() {
@@ -60,20 +97,28 @@ public class Scenario {
     }
 
     public void delete(int index) {
-        String allContents = readAll();
-        String[] scenarioList = allContents.split("\n");
+        String[] scenarioList = getScenarioList();
+        if (isInvalidReadIndex(index, scenarioList)) {
+            return;
+        }
         List<String> list = new ArrayList<>(Arrays.asList(scenarioList));
         list.remove(index);
-        scenarioList = list.toArray(new String[0]);
-        allContents = String.join("\n", scenarioList);
-        fileHandler.writeScenario(allContents);
+        writeScenario(list);
     }
 
     public void update(int index, String command) {
-        String allContents = readAll();
-        String[] scenarioList = allContents.split("\n");
-        scenarioList[index] = command;
-        allContents = String.join("\n", scenarioList);
-        fileHandler.writeScenario(allContents);
+        String[] scenarioList = getScenarioList();
+        if (isInvalidReadIndex(index, scenarioList)) {
+            return;
+        }
+        List<String> list = new ArrayList<>(Arrays.asList(scenarioList));
+        list.set(index, command);
+        writeScenario(list);
     }
+
+    private void writeScenario(List<String> list) {
+        String[] scenarioList = list.toArray(new String[0]);
+        fileHandler.writeScenario(String.join(LINE_SEPARATOR, scenarioList));
+    }
+
 }
--- src/test/java/ScenarioTest.java
+++ src/test/java/ScenarioTest.java
@@ -1,16 +1,8 @@
 import command.scenario.Scenario;
-import exception.ShellException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ScenarioTest {
@@ -20,17 +12,19 @@ public class ScenarioTest {
 
     @Test
     void testScenario() {
-        scenario.create("write 55 0x0000000A");
+        scenario.append("write 55 0x0000000A");
         scenario.read(0);
         scenario.insert(0, "read 55");
-        scenario.create("write 55 0x0000000A");
+        scenario.append("write 55 0x0000000A");
         scenario.update(0, "write 55 0x0000000A");
-        scenario.create("write 57 0x0000000A");
-        scenario.create("write 59 0x0000000A");
+        scenario.append("write 57 0x0000000A");
+        scenario.append("write 59 0x0000000A");
         scenario.deleteAll();
-        scenario.create("write 55 0x0000000A");
-        scenario.create("write 57 0x0000000A");
-        scenario.create("write 59 0x0000000A");
+        scenario.append("write 55 0x0000000A");
+        scenario.append("write 57 0x0000000A");
+        scenario.append("write 59 0x0000000A");
         scenario.insert(2, "read 55");
+        scenario.delete(2);
+        scenario.readAll();
     }
 }
