eba20faa151e6d42196951e675919a1d46e72888
chlduq04 <chlduq04@gmail.com>
2024-05-08 11:08:56 +0900

    [feature] testapp1, testapp2 실행에 대한 RunnerTest.java 추가 Shell.java에 testapp1, testapp2 함수 추가

--- src/main/java/Shell.java
+++ src/main/java/Shell.java
@@ -24,12 +24,50 @@ public class Shell {
         }
     }
 
+    private void testApp1() throws Exception {
+        run("fullwrite 0xABCDFFFF");
+        run("fullread");
+    }
+
+    private void testApp2() throws Exception {
+        for (int i = 0; i < 30 ; i++) {
+            run("write 0 0xAAAABBBB");
+            run("write 1 0xAAAABBBB");
+            run("write 2 0xAAAABBBB");
+            run("write 3 0xAAAABBBB");
+            run("write 4 0xAAAABBBB");
+            run("write 5 0xAAAABBBB");
+        }
+
+        run("write 0 0x12345678");
+        run("write 1 0x12345678");
+        run("write 2 0x12345678");
+        run("write 3 0x12345678");
+        run("write 4 0x12345678");
+        run("write 5 0x12345678");
+
+        run("read 0");
+        run("read 1");
+        run("read 2");
+        run("read 3");
+        run("read 4");
+        run("read 5");
+    }
+
     public void run(String commandLine) throws Exception {
         if (isValidCommandLine(commandLine))
             return;
 
         ArrayList<String> commandOptionList = new ArrayList<>(Arrays.asList(commandLine.trim().split(COMMAND_SEPARATOR)));
         String commandStr = commandOptionList.get(0);
+
+        if (commandStr.equals("testapp1")) {
+            testApp1();
+            return;
+        } else if (commandStr.equals("testapp2")) {
+            testApp2();
+            return;
+        }
 
         ShellCommand command = ShellCommandFactory.of(commandStr);
         if (!command.isValidCommand(commandOptionList)) {
---
+++ src/test/java/RunnerTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class RunnerTest {
+    @Mock
+    private Shell shell;
+
+    @Mock
+    private SSD ssd;
+
+    @Mock
+    private ShellCommand shellCommand;
+
+    ArrayList<String> arrayList;
+
+    @BeforeEach
+    void setUp() {
+        arrayList = new ArrayList<>();
+    }
+
+    @Test
+    void test_runner_testapp1_호출시_shell() throws Exception {
+        shell.run("testapp1");
+        verify(shell, times(1)).run(anyString());
+    }
+
+    @Test
+    void test_runner_testapp2_호출시_shell() throws Exception {
+        shell.run("testapp2");
+        verify(shell, times(1)).run(anyString());
+    }
+
+}
