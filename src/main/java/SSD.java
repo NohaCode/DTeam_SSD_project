55fa5a700f6f42a1a062cc5bac24974a798ab85c
chlduq04 <chlduq04@gmail.com>
2024-05-08 10:18:53 +0900

    [refactor] 수정된 에러메세지는 INVALID_COMMAND_MESSAGE로 통일했습니다

--- src/main/java/SSD.java
+++ src/main/java/SSD.java
@@ -5,10 +5,7 @@ public class SSD {
     public static final String CORRECT_VALUE_REGEX = "^0x[0-9A-F]{8}$";
     public static final String DEFAULT_VALUE = "0x00000000";
 
-    public static final String INVALID_INDEX_MESSAGE = "INVALID ADDRESS";
-    public static final String INVALID_VALUE_MESSAGE = "INVALID VALUE";
     public static final String INVALID_COMMAND_MESSAGE = "INVALID COMMAND";
-    public static final String INVALID_LENGTH_PARAMETER_MESSAGE = "INVALID LENGTH PARAMETER";
 
     public static final String READ_COMMAND_SHORTCUT = "R";
     public static final String WRITE_COMMAND_SHORTCUT = "W";
@@ -40,7 +37,7 @@ public class SSD {
             throw new SSDException(INVALID_COMMAND_MESSAGE);
         }
 
-        if (command.isValidCommand(commandOptionList)) {
+        if (!command.isValidCommand(commandOptionList)) {
             throw new SSDException(INVALID_COMMAND_MESSAGE);
         }
 
