619bee172f186db5d5a5be1846ecc7f43341c007
kwangmin-p <96bgm@korea.ac.kr>
2024-05-10 08:31:48 +0900

    [feature] RUNNER - 파일에는 log를 찍으나 print는 하지 않음

--- src/main/java/command/shell/ShellCommand.java
+++ src/main/java/command/shell/ShellCommand.java
@@ -10,9 +10,14 @@ import util.Logger;
 import static app.SSD.INVALID_COMMAND_MESSAGE;
 
 public abstract class ShellCommand {
-    Logger logger = Logger.get(Logger.PRODUCTION);
     protected abstract boolean isValidCommandImpl(ArrayList<String> commandOptionList);
     protected abstract void runImpl(SSD ssd, ArrayList<String> commandOptionList);
+
+    static Logger logger = Logger.get(Logger.PRODUCTION);
+
+    public void setLogger(Logger logger){
+        ShellCommand.logger = logger;
+    }
 
     public boolean isValidCommand(ArrayList<String> commandOptionList){
         logger.log(makeCommandMessage(commandOptionList), this.getClass());
