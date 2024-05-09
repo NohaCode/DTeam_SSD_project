f9bdad86c812302817095351d20e0e1c0460debe
chlduq04 <chlduq04@gmail.com>
2024-05-09 09:36:49 +0900

    [feature] rebuild scenario 함수를 command로 추가 예정

--- src/main/java/command/shell/ShellCommandFactory.java
+++ src/main/java/command/shell/ShellCommandFactory.java
@@ -11,32 +11,40 @@ public class ShellCommandFactory {
                 commandMap.put(command, new ShellReadCommand());
             } else if (command.equals("write")) {
                 commandMap.put(command, new ShellWriteCommand());
-            } else if(command.equals("exit")){
+            } else if (command.equals("exit")) {
                 commandMap.put(command, new ShellExitCommand());
-            }
-            else if(command.equals("help")){
+            } else if (command.equals("help")) {
                 commandMap.put(command, new ShellHelpCommand());
-            }
-            else if(command.equals("fullwrite")){
+            } else if (command.equals("fullwrite")) {
                 commandMap.put(command, new ShellFullWriteCommand());
-            }
-            else if(command.equals("fullread")){
+            } else if (command.equals("fullread")) {
                 commandMap.put(command, new ShellFullReadCommand());
-            }
-            else if(command.equals("erase")){
+            } else if (command.equals("erase")) {
                 commandMap.put(command, new ShellEraseCommand());
-            }
-            else if (command.equals("testapp1")) {
+            } else if (command.equals("testapp1")) {
                 commandMap.put(command, new ShellTestApp1Command());
-            }
-            else if (command.equals("testapp2")) {
+            } else if (command.equals("testapp2")) {
                 commandMap.put(command, new ShellTestApp2Command());
-            }
-            else if(command.equals("erase_range")){
+            } else if (command.equals("erase_range")) {
                 commandMap.put(command, new ShellEraseRangeCommand());
-            }
-            else if(command.equals("run_list.lst")){
+            } else if (command.equals("run_list.lst")) {
                 commandMap.put(command, new ShellRunnerCommand());
+            } else if (command.equals("scenario_append")) {
+
+            } else if (command.equals("scenario_insert")) {
+
+            } else if (command.equals("scenario_read")) {
+
+            } else if (command.equals("scenario_read_all")) {
+
+            } else if (command.equals("scenario_read_range")) {
+
+            } else if (command.equals("scenario_update")) {
+
+            } else if (command.equals("scenario_delete")) {
+
+            } else if (command.equals("scenario_delete_all")) {
+
             }
         }
         return commandMap.get(command);
