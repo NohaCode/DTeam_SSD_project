d9eaf3f7bf485f7de598e78d70fcdaa55fe7ac24
chlduq04 <chlduq04@gmail.com>
2024-05-09 10:36:16 +0900

    [feature] rebuild 기능 개발 scenario delete all 추가

---
+++ src/main/java/command/shell/ShellScenarioDeleteAllCommand.java
@@ -0,0 +1,26 @@
+package command.shell;
+
+import app.SSD;
+import util.FileHandler;
+
+import java.util.ArrayList;
+
+public class ShellScenarioDeleteAllCommand implements ShellCommand {
+    @Override
+    public boolean isValidCommand(ArrayList<String> commandOptionList) {
+        if (!isValidCommandOptionListSize(commandOptionList)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidCommandOptionListSize(ArrayList<String> commandOptionList) {
+        return commandOptionList.size() != 2;
+    }
+
+    @Override
+    public void run(SSD ssd, ArrayList<String> commandOptionList) {
+        FileHandler fileHandler = FileHandler.get();
+        fileHandler.writeScenario("");
+    }
+}
